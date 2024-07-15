637ad2de24b69bf320c2666f0eb18fb72d584566
john <johnzorrillx@gmail.com>
2024-07-15 13:18:59 -0500

    aaa

--- views/layout.php
+++ views/layout.php
@@ -1,35 +1,19 @@
-<?php
-require_once 'middleware/AuthorizationMiddleware.php';
-
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
-
-if (!isset($_SESSION['user_id'])) {
-    header('Location: index.php?controller=auth&action=login');
-    exit;
-}
-
-
-$role_id = $_SESSION['role'];
-
-$userId = $_SESSION['user_id'];
-
-$db = connectDatabase();
-$db = connectDatabase();
-$authorizationMiddleware = new AuthorizationMiddleware(new UserRole($db), new Permission($db));
-$canCreateUser = $authorizationMiddleware->checkPermission($userId, 'create_user');
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <link rel="stylesheet" href="views/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
     <title>Inventariado</title>
 </head>
 <body>
@@ -58,11 +42,9 @@ $canCreateUser = $authorizationMiddleware->checkPermission($userId, 'create_user
                     <li class="nav-item">
                         <a class="nav-link" href="index.php?controller=unidad_didactica&action=index">Unidad Didáctica</a>
                     </li>
-                    <?php if ($canCreateUser):  ?>
                         <li class="nav-item">
                             <a class="nav-link" href="index.php?controller=usuario&action=index">Usuarios</a>
                         </li>
-                    <?php endif; ?>
                     <li class="nav-item">
                         <a class="nav-link" href="index.php?controller=auth&action=logout">Cerrar sesión</a>
                     </li>
