37bff7ed8a0815caec3c744f3b44954f358163b4
john <johnzorrillx@gmail.com>
2024-07-19 17:24:01 -0500

    design user.css y categoria.css

--- views/categoria/index.php
+++ views/categoria/index.php
@@ -1,4 +1,8 @@
+<?php
+require_once 'models/salon.php';
+?>
 <link rel="stylesheet" href="views/categoria/categoria.css">
+
 <div class="container">
     <h2 class="poppins-bold">Lista de Categorías</h2>
     <?php if ($rol != 3): // 3 es el rol de Profesor ?>
@@ -6,15 +10,17 @@
     <?php endif; ?>
     <div class="row">
         <?php foreach ($categorias as $categoria): ?>
-            <div class="col-md-4 mb-4">
+            <div class="col-md-4">
                 <div class="card">
                     <img src="images/fuente450W.jpg" class="card-img-top" alt="Imagen de <?php echo htmlspecialchars($categoria['nombre_categoria'], ENT_QUOTES, 'UTF-8'); ?>">
                     <div class="card-body">
                         <h5 class="card-title poppins-bold"><?php echo htmlspecialchars($categoria['nombre_categoria'], ENT_QUOTES, 'UTF-8'); ?></h5>
-                        <a href="index.php?controller=item&action=index&id_categoria=<?php echo htmlspecialchars($categoria['id_categoria'], ENT_QUOTES, 'UTF-8'); ?>" class="btn btn-primary">Ver Items</a>
-                        <?php if ($rol != 3): ?>
-                            <a href="index.php?controller=categoria&action=edit&id=<?php echo htmlspecialchars($categoria['id_categoria'], ENT_QUOTES, 'UTF-8'); ?>" class="btn btn-primary">Editar</a>
-                        <?php endif; ?>
+                        <div class="card-buttons">
+                            <a href="index.php?controller=item&action=index&id_categoria=<?php echo htmlspecialchars($categoria['id_categoria'], ENT_QUOTES, 'UTF-8'); ?>" class="btn btn-primary">Ver Items</a>
+                            <?php if ($rol != 3): ?>
+                                <a href="index.php?controller=categoria&action=edit&id=<?php echo htmlspecialchars($categoria['id_categoria'], ENT_QUOTES, 'UTF-8'); ?>" class="btn btn-primary">Editar</a>
+                            <?php endif; ?>
+                        </div>
                     </div>
                 </div>
             </div>
--- views/usuario/index.php
+++ views/usuario/index.php
@@ -1,28 +1,34 @@
-<div class="container">
-    <h2 class="poppins-bold">Lista de Usuarios</h2>
-    <a href="index.php?controller=usuario&action=create" class="btn btn-success mb-3 poppins-regular">Crear Usuario</a>
-    
-    <div class="row">
-        <!-- Tarjeta para Asistentes -->
-        <div class="col-md-6 mb-4">
-            <div class="card card-gradient text-center">
-                <a href="index.php?controller=asistente&action=index" class="text-decoration-none text-dark">
-                    <div class="card-body">
-                        <h5 class="card-title poppins-bold">Asistentes</h5>
-                    </div>
-                </a>
+<link rel="stylesheet" href="views/usuario/usuario.css">
+<body>
+    <div class="container">
+        <h2 class="poppins-bold">Lista de Usuarios</h2>
+        <a href="index.php?controller=usuario&action=create" class="btn btn-success mb-3 poppins-regular">Crear Usuario</a>
+        
+        <div class="row justify-content-center">
+            <!-- Tarjeta para Asistentes -->
+            <div class="col-md-4 mb-4">
+                <div class="card card-gradient text-center d-flex align-items-center justify-content-center">
+                    <a href="index.php?controller=asistente&action=index" class="text-decoration-none text-dark w-100 h-100 d-flex align-items-center justify-content-center">
+                        <div class="card-body">
+                            <h5 class="card-title poppins-bold">Asistentes</h5>
+                        </div>
+                    </a>
+                </div>
             </div>
-        </div>
 
-        <!-- Tarjeta para Profesores -->
-        <div class="col-md-6 mb-4">
-            <div class="card card-gradient text-center">
-                <a href="index.php?controller=profesor&action=index" class="text-decoration-none text-dark">
-                    <div class="card-body">
-                        <h5 class="card-title poppins-bold">Profesores</h5>
-                    </div>
-                </a>
+            <!-- Tarjeta para Profesores -->
+            <div class="col-md-4 mb-4">
+                <div class="card card-gradient text-center d-flex align-items-center justify-content-center">
+                    <a href="index.php?controller=profesor&action=index" class="text-decoration-none text-dark w-100 h-100 d-flex align-items-center justify-content-center">
+                        <div class="card-body">
+                            <h5 class="card-title poppins-bold">Profesores</h5>
+                        </div>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
-</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
