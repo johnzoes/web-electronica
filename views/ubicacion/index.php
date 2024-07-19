687113e524d9f134adddd1adb9bccbe0757197ab
leo <leopb77@gmail.com>
2024-07-18 21:51:39 -0500

    Elementos ocultos para profesores en Ubicacion

--- views/ubicacion/index.php
+++ views/ubicacion/index.php
@@ -1,17 +1,27 @@
 <?php
 require_once 'models/salon.php';
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+$rol = isset($_SESSION['role']) ? $_SESSION['role'] : null;
 ?>
 <link rel="stylesheet" href="styles.css">
 
 <div class="container">
     <h2 class="my-4 poppins-bold">Lista de Ubicaciones</h2>
-    <a href="index.php?controller=ubicacion&action=create" class="btn btn-success mb-3">Crear Ubicación</a>
+    <?php if ($rol != 3): // Si no es profesor ?>
+        <a href="index.php?controller=ubicacion&action=create" class="btn btn-success mb-3">Crear Ubicación</a>
+    <?php endif; ?>
     <table class="table-modern" id="sortableTable">
         <thead>
             <tr>
                 <th data-sort="name">Nombre del Armario</th>
                 <th data-sort="salon">Salón</th>
-                <th>Acciones</th>
+                <?php if ($rol != 3): // Si no es profesor ?>
+                    <th>Acciones</th>
+                <?php endif; ?>
             </tr>
         </thead>
         <tbody>
@@ -19,14 +29,16 @@ require_once 'models/salon.php';
                 <tr>
                     <td><?php echo htmlspecialchars($ubicacion['nombre_armario'], ENT_QUOTES, 'UTF-8'); ?></td>
                     <td><?php echo htmlspecialchars($ubicacion['id_salon'], ENT_QUOTES, 'UTF-8'); ?></td>
-                    <td>
-                    <a href="index.php?controller=ubicacion&action=edit&id=<?php echo htmlspecialchars($ubicacion['id_ubicacion'], ENT_QUOTES, 'UTF-8'); ?>" class="btn btn-primary btn-sm">
-                            <img src="images/icons8-editar.svg" alt="Editar" width="25" height="25">
-                        </a>                  
-                    <button type="submit" class="btn">
+                    <?php if ($rol != 3): // Si no es profesor ?>
+                        <td>
+                            <a href="index.php?controller=ubicacion&action=edit&id=<?php echo htmlspecialchars($ubicacion['id_ubicacion'], ENT_QUOTES, 'UTF-8'); ?>" class="btn btn-primary btn-sm">
+                                <img src="images/icons8-editar.svg" alt="Editar" width="25" height="25">
+                            </a>
+                            <button type="submit" class="btn">
                                 <img src="images/icons8-eliminar.svg" alt="Eliminar" width="25" height="25">
                             </button>
-                    </td>
+                        </td>
+                    <?php endif; ?>
                 </tr>
             <?php endforeach; ?>
         </tbody>
