51b519a0f414df75805f7872817d56cc593d4615
john <johnzorrillx@gmail.com>
2024-07-20 14:49:08 -0500

    aa

--- views/reserva/mis_reservas.php
+++ views/reserva/mis_reservas.php
@@ -19,7 +19,7 @@
                 <td><?php echo htmlspecialchars($reserva['nombre_unidad_didactica'], ENT_QUOTES, 'UTF-8'); ?></td>
                 <td><?php echo htmlspecialchars($reserva['nombre_turno'], ENT_QUOTES, 'UTF-8'); ?></td>
                 <td>
-                    <a href="index.php?controller=reserva&action=edit&id=<?php echo htmlspecialchars($reserva['id_reserva'], ENT_QUOTES, 'UTF-8'); ?>" class="btn btn-primary btn-sm">Editar</a>
+                    
                     <form action="index.php?controller=reserva&action=delete&id=<?php echo htmlspecialchars($reserva['id_reserva'], ENT_QUOTES, 'UTF-8'); ?>" method="POST" style="display: inline-block;">
                         <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>
                     </form>
